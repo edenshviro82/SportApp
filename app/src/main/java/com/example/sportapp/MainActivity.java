50fb8f9087c85ca295f5e82fbc17e4708e9852a4
lital.hekmati <litalhechmati@gmail.com>
2022-12-21 15:25:45 +0200

    first fragment

--- app/src/main/java/com/example/sportapp/MainActivity.java
+++ app/src/main/java/com/example/sportapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.sportapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,7 +11,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    NavController navController;
     Button signIn,signUp;
 
     @Override
@@ -16,19 +19,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        signIn=findViewById(R.id.mainAc_SignIn_btn);
-        signUp=findViewById(R.id.mainAc_SignUp_btn);
+
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.main_navhost);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupActionBarWithNavController(this,navController);
 
 
-        signIn.setOnClickListener((view -> {
-            Intent signInIntent= new Intent(this, SignInActivity.class);
-            startActivity(signInIntent);
-        }));
-
-        signUp.setOnClickListener((view -> {
-            Intent signUpIntent= new Intent(this, SignUpActivity.class);
-            startActivity(signUpIntent);
-        }));
+//        signIn=findViewById(R.id.mainAc_SignIn_btn);
+//        signUp=findViewById(R.id.mainAc_SignUp_btn);
+//
+//
+//        signIn.setOnClickListener((view -> {
+//            Intent signInIntent= new Intent(this, SignInActivity.class);
+//            startActivity(signInIntent);
+//        }));
+//
+//        signUp.setOnClickListener((view -> {
+//            Intent signUpIntent= new Intent(this, SignUpActivity.class);
+//            startActivity(signUpIntent);
+//        }));
 
     }
 }
